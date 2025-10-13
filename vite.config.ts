4bf3f30fa860a5a0ce17eb12dbeab58333ce41e7
ambrabenvenuti <abenvenuti@onitsmart.it>
2025-10-13 12:49:33 +0200

    nominato il tema "onit-custom"

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         react(),
         keycloakify({
             accountThemeImplementation: "none",
-            // themeName: ["onit-custom"]
+            themeName: "onit-custom"
         })
     ]
 });
